9112e8d90b171d55ade1a2f634f25259deae3c4e
Richard T. Weeks <rtweeks21@gmail.com>
2021-03-14 16:43:49 -0700

    Fix a bunch of bad assumptions about versioned S3 storage
    ...and other bugs with quilting.
    
    * Identify the right version for the content by MD5 hash *of the actual
    content uploaded* (i.e. post-encoding hash).
    * Include the `Metadata` keyword in `extra_args` (which got lost in some
    previous delta).
    * Handle the streaming from the "target" file with a class.
    * Include the S3 object version in the file manifest data.

--- src/s3_sysbackup/quilting.py
+++ src/s3_sysbackup/quilting.py
@@ -17,15 +17,20 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from binascii import hexlify, unhexlify
 import boto3 # package boto3
 from botocore.exceptions import ClientError as AwsError # package boto3
 from contextlib import ExitStack
+from collections import defaultdict
+import functools
 import gzip
 import hashlib
+import io
 import json
 import os.path
 import shutil
 from tempfile import TemporaryFile
+from typing import List
 
 from .fs import (
     compresses_well,
@@ -66,6 +71,9 @@ class TargetFileError(Exception):
             return "target {!r} not found on system".format(self.file)
         return "unable to open target {!r}".format(self.file)
 
+class _NonconformingLatestVersionError(Exception):
+    """Raised if a mismatch between object key and content is detected"""
+
 class Quilter(BackupFacilitator):
     def __init__(self, bucket: str, *,
         manifest_prefix: str = '',
@@ -78,6 +86,7 @@ class Quilter(BackupFacilitator):
         self.manifest = {}
         self.s3 = s3_client or boto3.client('s3')
         self.lock_timeout = lock_timeout
+        self._retained_versions_info = None
     
     def add(self, fpath):
         _log.info("picking file at %r", fpath)
@@ -98,34 +107,62 @@ class Quilter(BackupFacilitator):
         # Make a local, temporary copy of the file (holding a flock on the source for the duration)
         with ExitStack() as resources:
             f = resources.enter_context(TemporaryFile())
+            extra_args = {'Metadata': {}}
+            extra_args.update(self._retention_args(prefix='ObjectLock'))
             
             # TODO: Try to use reflink to make a copy-on-write (i.e. lightweight)
-            # copy of the file to work with instead of an actual file-copy operation
-            sha256hash = hashlib.sha256()
-            md5_hash = S3IntegrityHasher()
-            file_hasher = FileHasher(sha256hash, md5_hash)
-            with self._open_target_file(fpath) as origf:
-                with backup_flock(origf, self.lock_timeout) as read_lock:
-                    locked_for_read = read_lock.obtained
-                    while True:
-                        buf = origf.read(256 * 1024)
-                        if not buf:
-                            break
-                        f.write(buf)
-                        file_hasher.update(buf)
+            # copy of the file as a starting point to avoid a "partial write";
+            # still do this in the context of a LOCK_SH flock on the file (if
+            # quickly available).
+            with self._open_target_file(fpath) as origf, backup_flock(origf, self.lock_timeout) as read_lock:
+                locked_for_read = read_lock.obtained
+                backup_content_info = _FilePrepStream(origf).stream_to(f.write)
             f.seek(0)
             
-            compress_content = compresses_well(f)
-            f.seek(0)
+            if backup_content_info.content_compressed:
+                _log.info("gzip-compressing content from %r", fpath)
+                extra_args['Metadata']['storage-encoding'] = 'gzip'
+                if _log.isEnabledFor(logging.INFO):
+                    sizes = (zf.tell(), f.tell())
+                    ratio = 1 - (sizes[0] / sizes[1])
+                    _log.info(
+                        "compressed %d byes to %d bytes (%.1f%%)",
+                        backup_content_info.input_byte_count,
+                        backup_content_info.output_byte_count,
+                        backup_content_info.compression_ratio * 100,
+                    )
             
-            content_key = "content/" + sha256hash.hexdigest()
+            content_key = "content/" + backup_content_info.content_hexdigest
             _log.info("content key for %r: %s", fpath, content_key)
             
             object_loc = dict(Bucket=self.bucket, Key=content_key)
             
             try:
+                version_records = self._get_content_version_records(
+                    content_key
+                )
+                existing_version_id = None
+                valid_versions = list(
+                    v_rec['VersionId'] for v_rec in version_records
+                    if backup_content_info.matches_etag(v_rec['ETag'])
+                )
+                
+                # If there is at least one INvalid version, log an error requesting manual review
+                if len(version_recs) > len(valid_versions):
+                    _log.error("Key %r has more than one stored version; manual review of versions suggested", content_key)
+                
+                # If there is at least one valid version, try extending that version's retention period
+                if len(valid_versions) > 0:
+                    existing_version_id = valid_versions[0]
+                else:
+                    # Otherwise, we'll have to upload
+                    raise _NonconformingLatestVersionError(self.bucket, content_key)
+                
+                # TODO: Use a local database to check if we know we set a long-enough retention period
+                
                 update_retention_kwargs = dict(
                     **object_loc,
+                    VersionId=existing_version_id,
                     Retention=self._retention_args(),
                 )
                 _log.debug(
@@ -138,24 +175,11 @@ class Quilter(BackupFacilitator):
                     self.bucket,
                     content_key
                 )
-            except AwsError as e:
-                # Sadly, we can't test for a "NoSuchKey" error as S3 sometimes returns an "AccessDenied" instead
+                version_id = existing_version_id
                 
-                if compress_content:
-                    _log.info("gzip-compressing content from %r", fpath)
-                    zf = resource.enter_context(TemporaryFile())
-                    shutil.copyfileobj(f, gzip.GzipFile(None, 'wb', fileobj=zf))
-                    if _log.isEnabledFor(logging.INFO):
-                        sizes = (zf.tell(), f.tell())
-                        ratio = 1 - (sizes[0] / sizes[1])
-                        _log.info(
-                            "compressed %d byes to %d bytes (%.1f%%)",
-                            *sizes,
-                            ratio * 100,
-                        )
-                    f.close()
-                    zf.seek(0)
-                    f = zf
+                # TODO: Update the local database to let it know we set the retention period
+            except (AwsError, _NonconformingLatestVersionError) as e:
+                # Sadly, we can't test for a "NoSuchKey" error as S3 sometimes returns an "AccessDenied" instead
                 
                 _log.info(
                     "storing file content for %r at s3://%s/%s",
@@ -163,13 +187,16 @@ class Quilter(BackupFacilitator):
                     object_loc['Bucket'],
                     object_loc['Key'],
                 )
-                self.s3.put_object(
+                put_object_response = self.s3.put_object(
                     **object_loc,
                     Body=f,
                     ContentMD5=md5_hash.aws_integrity(),
                     **extra_args,
                 )
                 _log.info("content of %r stored in S3", fpath)
+                version_id = put_object_response['VersionId']
+                
+                # TODO: Update the local database with the new retention period information for this object
         
         # Accumulate this file into the manifest
         _log.info("incorporating file %r in manifest", fpath)
@@ -177,6 +204,7 @@ class Quilter(BackupFacilitator):
             get_ownership(fpath),
             get_permissions(fpath),
             content_key,
+            version_id,
             storage_id(fpath),
         )
         if not locked_for_read:
@@ -234,6 +262,29 @@ class Quilter(BackupFacilitator):
             return open(file, **kwargs)
         except (FileNotFoundError, PermissionError) as e:
             raise TargetFileError(file) from e
+    
+    def _get_content_version_records(self, content_key: str) -> List[str]:
+        self._load_content_listing()
+        return self._retained_versions_info.get(content_key, [])
+    
+    # MINIMIZE LIST CALLS!!!
+    def _load_content_listing(self):
+        if self._retained_versions_info is not None:
+            return
+        
+        self._retained_versions_info = ver_info_dict = {}
+        lov_kwargs = dict(
+            Bucket=self.bucket,
+            Prefix='content/',
+        )
+        while lov_kwargs.get('KeyMarker', True):
+            lov_response = self.s3.list_object_versions(**lov_kwargs)
+            # Prepare for next iteration
+            lov_kwargs['KeyMarker'] = lov_response.get('NextKeyMarker', '')
+            lov_kwargs['VersionIdMarker'] = lov_response.get('NextVersionIdMarker', '')
+            
+            for ver_rec in lov_response['Versions']:
+                ver_info_dict.setdefault(ver_rec['Key'], []).append(ver_rec)
 
 class Symlink:
     def __init__(self, ownership, target):
@@ -262,7 +313,7 @@ class Directory:
 class File:
     unlocked_copy = False
     
-    def __init__(self, ownership, perms_dict, content_key, storage_id):
+    def __init__(self, ownership, perms_dict, content_key, version_id, storage_id):
         super().__init__()
         self.ownership = ownership
         self.perms_dict = {}
@@ -271,6 +322,7 @@ class File:
                 self.perms_dict[k] = perms_dict[k]
                 break
         self.content_key = content_key
+        self.version_id = version_id
         self.storage_id = storage_id
     
     @property
@@ -280,8 +332,101 @@ class File:
             **self.ownership,
             **self.perms_dict,
             'dat': self.content_key,
+            'ver': self.version_id,
             'id': self.storage_id,
         }
         if self.unlocked_copy:
             result.setdefault('w', []).append('unlocked-copy')
         return result
+
+class _FilePrepStream:
+    def __init__(self, source):
+        super().__init__()
+        self.sha256_prehash = hashlib.sha256()
+        self.md5_prehash = hashlib.sha256()
+        self.input_byte_count = 0
+        self.md5_posthash = hashlib.md5()
+        self.output_byte_count = 0
+        self._source = source
+    
+    class Result:
+        content_compressed = False
+        sha256_prehash = None
+        md5_prehash = None
+        input_byte_count = None
+        md5_posthash = None
+        output_byte_count = None
+        
+        @property
+        def content_hexdigest(self) -> str:
+            return hexlify(self.sha256_prehash).decode('ascii')
+        
+        def aws_integrity(self, ) -> str:
+            return b64encode(self.md5_posthash).decode('ascii')
+        
+        def matches_etag(self, etag) -> bool:
+            if etag[0] != etag[-1] and etag[0] not in '"\'':
+                return False
+            etag_val = unhexlify(etag[1:-1])
+            return etag_val in (self.md5_prehash, self.md5_posthash)
+        
+        @property
+        def compression_ratio(self):
+            return 1 - (self.output_byte_count / self.input_byte_count)
+    
+    @property
+    def source(self):
+        return self._source
+    
+    def stream_to(self, consume: Callable[[bytes], Any], *, chunksize=1<<18):
+        result = self.Result()
+        buf = self.source.read(chunksize)
+        
+        compress_stream = compresses_well(io.BytesIO(buf))
+        if compress_stream:
+            result.content_compressed = True
+            consume_gzipped = functools.partial(self._posthash_and_consume, consume)
+            zf = gzip.GzipFile(None, 'wb', fileobj=self._Adapter(consume_gzipped))
+            consume = lambda buf: zf.write(buf) if buf else zf.flush()
+        else:
+            consume = functools.partial(self._posthash_and_consume, consume)
+        
+        self.sha256_prehash.update(buf)
+        self.md5_prehash.update(buf)
+        self.input_byte_count += len(buf)
+        consume(buf)
+        
+        for buf in self._remaining_chunks(self.source, chunksize):
+            self.sha256_prehash.update(buf)
+            self.md5_prehash.update(buf)
+            self.input_byte_count += len(buf)
+            consume(buf)
+        
+        consume(b'')
+        
+        result.sha256_prehash, self.sha256_prehash = self.sha256_prehash.digest(), hashlib.sha256()
+        result.md5_prehash, self.md5_prehash = self.md5_prehash.digest(), hashlib.md5()
+        result.md5_posthash, self.md5_posthash = self.md5_posthash.digest(), hashlib.md5()
+        result.input_byte_count, self.input_byte_count = self.input_byte_count, 0
+        result.output_byte_count, self.output_byte_count = self.output_byte_count, 0
+        return result
+    
+    def _posthash_and_consume(self, consume, content):
+        self.md5_posthash.update(content)
+        self.output_byte_count += len(content)
+        consume(content)
+    
+    class _Adapter:
+        def __init__(self, consume):
+            super().__init__()
+            self.write = consume
+        
+        def flush(self):
+            pass
+    
+    def _remaining_chunks(self, f, chunksize):
+        while True:
+            buf = f.read(chunksize)
+            if not buf:
+                break
+            yield buf
