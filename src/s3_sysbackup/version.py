a316b7195934f82610e0a6746e4e1cafbba28436
Richard T. Weeks <rtweeks21@gmail.com>
2021-08-12 13:54:03 -0700

    Bump version number to 0.2.0

--- src/s3_sysbackup/version.py
+++ src/s3_sysbackup/version.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-_nominal_version = "0.1.0"
+_nominal_version = "0.2.0"
 
 import os.path
 import subprocess
